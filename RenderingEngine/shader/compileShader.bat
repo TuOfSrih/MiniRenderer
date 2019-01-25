7d9a46d37e4370d152e0a2df7985f851342b9852
Florian Herick <f.herick@gmx.de>
2019-01-26 00:34:47 +0100

    Finally Debug Messaging working

--- RenderingEngine/shader/compileShader.bat
+++ RenderingEngine/shader/compileShader.bat
@@ -1,3 +1,4 @@
-C:\VulkanSDK\1.1.73.0\Bin32\glslangValidator.exe -V shader.vert
-C:\VulkanSDK\1.1.73.0\Bin32\glslangValidator.exe -V shader.frag
+cd C:\Users\Flo\Documents\RenderingEngine\RenderingEngine\RenderingEngine\shader
+C:\VulkanSDK\1.1.97.0\Bin32\glslangValidator.exe -V shader.vert
+C:\VulkanSDK\1.1.97.0\Bin32\glslangValidator.exe -V shader.frag
 pause
